398602a20691c77275ccf5d33f77d94e9db544f9
Nicolas_Rotela <nicolas_rotela@hotmail.com>
2026-07-02 18:26:57 -0300

    preparando app para deploy

--- back/db/connection.js
+++ back/db/connection.js
@@ -2,13 +2,20 @@ import pkg from "pg";
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+    });
 
 pool
   .query("SELECT NOW()")
