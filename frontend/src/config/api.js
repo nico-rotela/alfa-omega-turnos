0e12545f229364d0dd5196802d94a187296feee4
Nicolas_Rotela <nicolas_rotela@hotmail.com>
2026-07-02 21:20:06 -0300

    preparando app para deploy

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,2 +1,3 @@
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL =
+  import.meta.env.VITE_API_URL || "https://alfa-omega-turnos.onrender.com";
 console.log("API_URL:", API_URL);
